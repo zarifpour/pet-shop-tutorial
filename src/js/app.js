57562517361755430ab4c0dfee570f96f62559fb
Daniel Zarifpour <dzarifpour@gmail.com>
2021-05-06 01:44:45 -0400

    Completed app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+const { default: Web3 } = require("web3");
+
 App = {
   web3Provider: null,
   contracts: {},
@@ -23,18 +25,42 @@ App = {
     return await App.initWeb3();
   },
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+  initWeb3: async function () {
+    if (window.ethereum) { // Indicates we are using a modern dapp browser/more recent version of MetaMask
+      App.web3provider = window.ethereum;
+      try {
+
+        await window.ethereum.enable()
+      } catch (error) {
+        
+        console.error("User denied account access");
+      }
+    }
+
+    else if (window.web3) { // Indicates we are using an older dapp browser
+      App.web3provider = window.web3.currentProvider;
+    }
+
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545')
+    }
+    web3 = new Web3(App.web3Provider); // Fallback is suitable for development environments but insecure and not suitable for production
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Adoption.json', function (data) {
+
+      var AdoptionArtifact = data; // Info such as deployed address and Application Binary Interface (ABI)
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
@@ -44,9 +70,21 @@ App = {
   },
 
   markAdopted: function() {
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function (instance) {
+      adoptionInstance = instance;
+
+      return adoptionInstance.getAdopters.call(); // Using call() allows us to read data from the blockchain without having to send a full transaction
+    }).then(function (adopters) {
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function (err) {
+      console.log(err.message);
+    });
   },
 
   handleAdopt: function(event) {
@@ -54,9 +92,26 @@ App = {
 
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function (error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function (instance) {
+        adoptionInstance = instance;
+
+
+        return adoptionInstance.adopt(petId, { from: account });
+      }).then(function (result) {
+        return App.markAdopted();
+      }).catch(function (err) {
+        console.log(err.message);
+      });
+    });
   }
 
 };
